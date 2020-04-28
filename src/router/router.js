2fa9443825f869dc90fe18e37f20e7b9640c6e9a
lucian <montreal.lucian@gmail.com>
2020-04-28 08:49:32 +0800

    update

--- src/router/router.js
+++ src/router/router.js
@@ -2,24 +2,24 @@ import React, { Component } from "react"
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 
 import Home from "component/Home/Home"
-import VISA from "component/Visa/Index"
-import Counter from "component/Counter/Counter"
-import UserInfo from "component/UserInfo/UserInfo"
+// import VISA from "component/Visa/Index"
+// import Counter from "component/Counter/Counter"
+// import UserInfo from "component/UserInfo/UserInfo"
 
 const AppRouter = () => (
     <div>
         <Router>
-            <ul>
+            {/* <ul>
                 <li><Link to='/'>首页</Link></li>
                 <li><Link to='/visa'>VISA</Link></li>
                 <li><Link to='/counter'>Counter</Link></li>
                 <li><Link to='/userInfo'>UserInfo</Link></li>
-            </ul>
+            </ul> */}
             <Switch>
                 <Route exact path="/" component={Home} />
-                <Route path="/visa" component={VISA} />
+                {/* <Route path="/visa" component={VISA} />
                 <Route path="/counter" component={Counter} />
-                <Route path="/userInfo" component={UserInfo} />
+                <Route path="/userInfo" component={UserInfo} /> */}
             </Switch>
         </Router>
     </div>
